1e6476d060bbc86416bcb8c5b5cf74c404f8420f
John Sanabria <algunsanabria@gmail.com>
2017-11-01 16:51:12 -0500

    Updated fa icons for more consistency

--- inc/pagination.php
+++ inc/pagination.php
@@ -48,13 +48,13 @@ function understrap_pagination() {
 	if ( ! in_array( 1, $links ) ) {
 		$class = 1 == $paged ? ' class="active page-item"' : ' class="page-item"';
 
-		printf( '<li %s><a class="page-link" href="%s"><i class="fa fa-step-backward" aria-hidden="true"></i></a></li>' . "\n",
+		printf( '<li %s><a class="page-link" href="%s"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a></li>' . "\n",
 		$class, esc_url( get_pagenum_link( 1 ) ), '1' );
 
 		/**    Previous Post Link */
 		if ( get_previous_posts_link() ) {
 			printf( '<li class="page-item page-item-direction page-item-prev"><span class="page-link">%1$s</span></li> ' . "\n",
-			get_previous_posts_link( '<span aria-hidden="true">&laquo;</span><span class="sr-only">Previous page</span>' ) );
+			get_previous_posts_link( '<i class="fa fa-angle-left" aria-hidden="true"></i><span class="sr-only">Previous page</span>' ) );
 		}
 
 		if ( ! in_array( 2, $links ) ) {
@@ -73,7 +73,7 @@ function understrap_pagination() {
 	// Next Post Link.
 	if ( get_next_posts_link() ) {
 		printf( '<li class="page-item page-item-direction page-item-next"><span class="page-link">%s</span></li>' . "\n",
-			get_next_posts_link( '<span aria-hidden="true">&raquo;</span><span class="sr-only">Next page</span>' ) );
+			get_next_posts_link( '<i class="fa fa-angle-right" aria-hidden="true"></i><span class="sr-only">Next page</span>' ) );
 	}
 
 	// Link to last page, plus ellipses if necessary.
@@ -83,7 +83,7 @@ function understrap_pagination() {
 		}
 
 		$class = $paged == $max ? ' class="active "' : ' class="page-item"';
-		printf( '<li %s><a class="page-link" href="%s" aria-label="Next"><span aria-hidden="true"><i class="fa fa-step-forward" aria-hidden="true"></i></span><span class="sr-only">%s</span></a></li>' . "\n",
+		printf( '<li %s><a class="page-link" href="%s" aria-label="Next"><span aria-hidden="true"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span><span class="sr-only">%s</span></a></li>' . "\n",
 		$class . '', esc_url( get_pagenum_link( esc_html( $max ) ) ), esc_html( $max ) );
 	}
 
